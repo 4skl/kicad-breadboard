ff1ca44a47856246b2a049e9ac21c69a3ba39b41
robin-cachy <robin@sveric.com>
2026-04-26 10:25:57 +0200

    Nano landscape: shift name+ref label above chip diamond

--- plugins/breadboard/canvas.py
+++ plugins/breadboard/canvas.py
@@ -2531,6 +2531,8 @@ class BreadboardCanvas(wx.Panel):
                 dc.SetFont(wx.Font(7, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
                                    wx.FONTWEIGHT_BOLD))
                 nw, nh = dc.GetTextExtent(dname)
+                # Nano: shift label above the chip diamond (chip_r=14 + margin).
+                lbl_cy = text_cy - (22 if is_nano else 0)
 
                 if ref:
                     dc.SetFont(wx.Font(6, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
@@ -2540,14 +2542,14 @@ class BreadboardCanvas(wx.Panel):
                     total_h = nh + gap + rh
                     dc.SetFont(wx.Font(7, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
                                        wx.FONTWEIGHT_BOLD))
-                    dc.DrawText(dname, text_cx - nw//2, text_cy - total_h//2)
+                    dc.DrawText(dname, text_cx - nw//2, lbl_cy - total_h//2)
                     dc.SetFont(wx.Font(6, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
                                        wx.FONTWEIGHT_NORMAL))
-                    dc.DrawText(ref,   text_cx - rw//2, text_cy - total_h//2 + nh + gap)
+                    dc.DrawText(ref,   text_cx - rw//2, lbl_cy - total_h//2 + nh + gap)
                 else:
                     dc.SetFont(wx.Font(7, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
                                        wx.FONTWEIGHT_BOLD))
-                    dc.DrawText(dname, text_cx - nw//2, text_cy - nh//2)
+                    dc.DrawText(dname, text_cx - nw//2, lbl_cy - nh//2)
 
         # ── Arduino Uno extras: split strips + ATmega + USB + barrel jack ──
         if comp_def.type_id == 'Arduino_Uno':
